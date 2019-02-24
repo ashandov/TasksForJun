62ba823731993870fbd3c492729742a9f7fd4476
Anatoly Shandov <anatolyshandov@gmail.com>
2019-02-24 21:02:57 +0300

    LinkedLists

---
+++ Task02/Task02/DoublyLinkedList/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DoblyLinkedList<int> list = new DoblyLinkedList<int>();
+			list.Add(3);
+			list.Add(4);
+			list.Add(5);
+			list.Add(6);
+			list.Add(7);
+			list.Remove(3);
+			list.AppendFirst(1);
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+	public class DoublyNode<T>
+	{
+		public DoublyNode(T data)
+		{
+			if (data == null)
+			{
+				throw new ArgumentNullException("Constructor couldn't take null");
+			}
+			Data = data;
+		}
+		public T Data { get; set; }
+		public DoublyNode<T> Next { get; set; }
+		public DoublyNode<T> Previous { get; set; }
+
+	}
+	class DoblyLinkedList<T>
+	{
+		private DoublyNode<T> head;
+		private DoublyNode<T> tail;
+		private int count;
+		public DoblyLinkedList()
+		{
+			count = 0;
+		}
+		public int Count { get { return count; } }
+		public void AppendFirst(T item)
+		{
+			DoublyNode<T> data = new DoublyNode<T>(item);
+			if (head != null)
+			{
+				head.Previous = data;
+				data.Next = head;
+				head = data;
+			}
+			else
+			{
+				head = data;
+				tail = data;
+			}
+			count++;
+		}
+		public void Add(T data)
+		{
+			DoublyNode<T> item = new DoublyNode<T>(data);
+			if (head == null)
+			{
+				head = item;
+			}
+			else
+			{
+				tail.Next = item;
+				item.Previous = tail;
+			}
+			tail = item;
+
+			count++;
+		}
+		public bool Remove(T data)
+		{
+			DoublyNode<T> current = head;
+			DoublyNode<T> previous = null;
+
+			DoublyNode<T> item = new DoublyNode<T>(data);
+			while (current != null)
+			{
+				if (current.Data.Equals(item.Data))
+				{
+					if (previous == null)
+					{
+						head = head.Next;
+						head.Previous = null;
+					}
+					else
+					{
+						if (current.Next == null)
+						{
+							tail = previous;
+						}
+						else
+						{
+							current.Next.Previous = previous;
+						}
+						previous.Next = current.Next;
+
+
+					}
+					count--;
+					return true;
+				}
+				else
+				{
+					previous = current;
+					current = current.Next;
+				}
+			}
+			return false;
+		}
+		public IEnumerator<T> GetEnumerator()
+		{
+			DoublyNode<T> item = head;
+			while (item != null)
+			{
+				yield return item.Data;
+				item = item.Next;
+			}
+		}
+
+	}
+}
+
---
+++ Task02/Task02/LinkedList/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LinkedList<string> list = new LinkedList<string>();
+			list.Add("First item");
+			list.Add("Second item");
+			list.Add("3rd item");
+			list.AppendFirst("Append first element");
+			list.Remove("Second item");
+			Console.WriteLine(list.Contains("3rd item"));
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+	class LinkedList<T>	
+	{
+		private Item<T> head;
+		private Item<T> tail;
+		public int count;
+		public LinkedList()
+		{
+			count = 0;
+
+		}
+		public void AppendFirst(T data)
+		{
+			Item<T> item = new Item<T>(data);
+			if (head == null)
+			{
+				head = item;
+				tail.Next = item;
+				tail = head;
+			}
+			else
+			{
+				item.Next = head;
+				head = item;
+			}
+			count++;
+
+		}
+		public void Add(T data)
+		{
+			Item<T> item = new Item<T>(data);
+			if (head == null)
+			{
+				head = item;
+			}
+			else
+			{
+				tail.Next = item;
+			}
+			tail = item;
+			count++;
+
+		}
+		public bool Remove(T data)
+		{
+			Item<T> current = head;
+			Item<T> previous = null;
+			while (current != null)
+			{
+				if (current.Data.Equals(data))
+				{
+					if (previous != null)
+					{
+						previous.Next = current.Next;
+						if (current.Next == null)
+						{
+							tail = previous;
+						}
+					}
+					else
+					{
+						head = head.Next;
+					}
+					if (head == null)
+					{
+						tail = null;
+					}
+					count--;
+					return true;
+				}
+				previous = current;
+				current = current.Next;
+			}
+			return false;
+		}
+		public bool Contains(T item)
+		{
+			Item<T> searchValue=head;
+			while(searchValue!=null)
+			{
+			   if(searchValue.Data.Equals(item))
+			   {
+				return true;
+			   }
+				   searchValue = searchValue.Next;
+			}
+			return false;
+		}
+		public IEnumerator<T> GetEnumerator()
+		{
+			Item<T> current = head;
+			while (current != null)
+			{
+				yield return current.Data;
+				current=current.Next;
+			}
+		}
+}
+	class Item<T>
+	{
+		public T Data { get; set; }
+		public Item(T data)
+		{
+			if (data == null)
+			{
+				throw new ArgumentNullException("Constructor couldn't take null");
+			}
+			Data = data;
+		}
+		public Item<T> Next { get; set; }
+
+	}
+}
